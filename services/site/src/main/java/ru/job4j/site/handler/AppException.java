58c8b8cd031a935102eda0b38354129c9fe78184
alexander-karmanov <lektor_md@mail.ru>
2026-03-09 15:17:57 +0300

    CheckDev - монолит - task#505058

---
+++ services/site/src/main/java/ru/job4j/site/handler/AppException.java
@@ -0,0 +1,8 @@
+package ru.job4j.site.handler;
+
+public class AppException extends RuntimeException {
+
+    public AppException(String message) {
+        super(message);
+    }
+}
---
+++ services/site/src/main/java/ru/job4j/site/handler/IdNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.job4j.site.handler;
+
+public class IdNotFoundException extends AppException {
+    public IdNotFoundException(String message) {
+        super(message);
+    }
+}
